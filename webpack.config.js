db3594079e36ca1da3e1f4a412dd93fbfc94999a
Junior Alves <jjalves96@gmail.com>
2020-01-21 09:25:21 -0300

    feat: first styled component

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,15 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }
+      }, {
+        test: /\.css$/, // Todos os arquivos que .css
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' }, 
+          { loader: 'css-loader' }, 
+
+        ]
+      } 
     ]
   }
 };
