9336a85c52ee1c978fbcd903f9a4de65f7844f43
Junior Alves <jjalves96@gmail.com>
2020-01-24 09:29:10 -0300

    feat: DefaultProps & PropTypes

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import PropTypes from 'prop-types'
+
 function TechItem ({ tech, onDelete }) {
   return (
     <li >
@@ -7,6 +9,16 @@ function TechItem ({ tech, onDelete }) {
       <button onClick={onDelete} type="button">Remover</button>
     </li>
   )
+}
+
+// Define valor padrão para tech, caso nao seja passado pelo o usuario
+TechItem.defaultProps = {
+  tech: 'Oculto',
+}
+
+TechItem.PropTypes = {
+  tech: PropTypes.string, // Define formato da prop "tech"
+  onDelete: PropTypes.func.isRequired, // Define que onDelete é uma funcao e é obrigatória
 }
 
 export default TechItem;
